1f646d784be3fc034ad424b5c6bc0220945139a5
vagrant <vagrant@localhost.localdomain>
2022-07-24 07:51:27 +0000

    update

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -33,7 +33,8 @@ class ProductsController < ApplicationController
     user = current_user.email
     @user = user
     #@products = current_user.products.includes(:product_stocks, recent_stock:{user: @user}, :recipes, :product_tracks).references(:product_stocks,  recent_stock:{user: @user}, :recipes, :product_tracks)
-    @products = current_user.products.includes(:product_stocks, :recipes, :product_tracks).references(:product_stocks, :recipes, :product_tracks)
+    @products = current_user.products.includes(:product_tracks).references(:product_tracks)
+    #@products = current_user.products.includes(:product_stocks, :recipes, :product_tracks).references(:product_stocks, :recipes, :product_tracks)
     #@stocks = current_user.product_stocks
     #@materials = current_user.materials
     #@recipes = current_user.recipes
