1f9bfd19d9b3e7aabe05aaf3c179f6e2e8fa4041
ecmerchant <murakami@ec-merchant.com>
2024-10-02 10:21:21 +0900

    redis.rb を更新

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,11 @@
-Resque.redis = Redis.new(:url => ENV['REDIS_URL'])
+if ENV["RAILS_ENV"] == "production"
+  redis_url = ENV["REDIS_URL"]
+else
+  redis_url = "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}/0"
+end
+Resque.redis = Redis.new(url: redis_url, ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+
+# Resque.redis = Redis.new(:url => ENV['REDIS_URL'])
 
 # Heroku RedisでRailsからRedisを使う場合
 # $redis = Redis.new(url: ENV["REDIS_URL"])
